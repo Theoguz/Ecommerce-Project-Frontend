4a08a9bb148bff10f4b3d954c1102d3906e0fbe0
Oguzz <oguzaslan_203@hotmail.com>
2024-03-07 20:39:20 +0300

    Code fix

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -13,26 +13,25 @@ export class ProductService {
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
-
-
   //localhost:8080/api/products?size=100' deleted from the base url // Spring Boot API and size=100 is to get all the products
   constructor(private httpClient: HttpClient) { }
+
+  getProduct(theProductId: number): Observable<Product> {
+    //need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     //TODO: need to build URL based on category id...will come back to this! done
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-
     return this.getProducts(searchUrl);
-
   }
 
   searchProducts(theKeyword: string): Observable<Product[]> {
-
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
-
     return this.getProducts(searchUrl);
   }
-
 
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponse>(searchUrl).pipe(
@@ -40,9 +39,7 @@ export class ProductService {
     );
   }
 
-
   getProductCategories(): Observable<ProductCategory[]> {
-
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
