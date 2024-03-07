835c8275e73a16d80d0e4b3f83a1dff6976ae6ba
Oguzz <oguzaslan_203@hotmail.com>
2024-03-07 07:00:42 +0300

    Product Categories added with Dynamic Search

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -2,28 +2,50 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products'; 
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
+
+
   //localhost:8080/api/products?size=100' deleted from the base url // Spring Boot API and size=100 is to get all the products
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(theCategoryId:number): Observable<Product[]>{
+  getProductList(theCategoryId: number): Observable<Product[]> {
     //TODO: need to build URL based on category id...will come back to this!
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-
     return this.httpClient.get<GetResponse>(searchUrl).pipe(
-      map(response => response._embedded.products) 
+      map(response => response._embedded.products)
     );
   }
+
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+
+  }
+
 }
-interface GetResponse{
-  _embedded:{
+
+interface GetResponse {
+  _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
