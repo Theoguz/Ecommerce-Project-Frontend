a6802f79cee7af305763b03293157626fc1bdfdb
Oguzz <oguzaslan_203@hotmail.com>
2024-03-09 07:01:21 +0300

    added some methods

--- angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -7,6 +7,7 @@ import { Subject } from 'rxjs';
 })
 export class CartService {
 
+
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>();  //publish events to all of the subscribers
@@ -72,5 +73,22 @@ export class CartService {
     }
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----------------');
+  }
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+  remove(theCartItem: CartItem) {
+    //get index of item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+    //if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
   }
 }
