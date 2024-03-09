ad92a10c0af05210c348971e8ebb7d9fcbb40954
Oguzz <oguzaslan_203@hotmail.com>
2024-03-09 23:52:46 +0300

    TotalPrice-Quantity property added

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -11,6 +11,8 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!: FormGroup;
   event: any;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
   constructor(private formBuilder: FormBuilder) { }
 
