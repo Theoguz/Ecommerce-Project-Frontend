f137421c892908d130ffe375bb091aabbdb2a039
Oguzz <oguzaslan_203@hotmail.com>
2024-03-11 06:43:32 +0300

    Login Okta config added

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId:'0oafp4hgt6Y9VZ6WJ5d7',
+        issuer:'https://dev-92975008.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',        
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
