4790ec420c00fd3a5465f5b205514a3578d7279f
Oguzz <oguzaslan_203@hotmail.com>
2024-03-12 16:09:29 +0300

    new config added

--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,10 +1,12 @@
+
 export default {
 
     oidc: {
-        clientId:'0oafp4hgt6Y9VZ6WJ5d7',
-        issuer:'https://dev-92975008.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',        
-        scopes: ['openid', 'profile', 'email']
+        clientId: '0oafpoth2s00FmSfg5d7',
+        issuer: 'https://dev-04878644.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email'],
+        ClassingEngine: true
     }
 
 }
