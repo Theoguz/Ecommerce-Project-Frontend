4399bb3f7576f226680c0a847475e96c0b7ecc7d
Oguzz <oguzaslan_203@hotmail.com>
2024-03-13 20:09:47 +0300

    sessionStorage email adress

--- angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = "";
 
+  storage: Storage = sessionStorage;
+
 
   constructor(private oktaAuthService: OktaAuthStateService,
     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
@@ -34,6 +36,12 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         (res) => {
           this.userFullName = res.name as string;
+
+          // Retrieve the user's email from authentication response
+          const theEmail = res.email as string;
+
+          // Store the email in the browser session storage
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
         }
       );
     }
