419f960f5b49a0acbb2b0f0e0f066cb6782d9984
Oguzz <oguzaslan_203@hotmail.com>
2024-03-13 20:09:02 +0300

    OrderHistory component generated

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { OKTA_CONFIG, OktaAuthGuard,  OktaAuthModule,  OktaCallbackComponent} fr
 import { OktaAuth } from '@okta/okta-auth-js';
 import  myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -63,7 +64,8 @@ const routes: Routes = [
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
-    MembersPageComponent
+    MembersPageComponent,
+    OrderHistoryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
