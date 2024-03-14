676b06f1a7a4d144ae85893e37f5ab3ad3ff6556
Oguzz <oguzaslan_203@hotmail.com>
2024-03-14 11:55:30 +0300

    StripeKey added

--- angular-ecommerce/src/environments/environment.ts
+++ angular-ecommerce/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  lu2shopApiUrl: 'https://localhost:8443/api',
+  luv2shopApiUrl: 'https://localhost:8443/api',
+  stripePublishableKey: "pk_test_51Ou69UJ4AAVmjh1E3QfZA5Xz9iIZccnwM4xIoID8TjAqV6YqjYmFHax0fG04xJviJH7ERvFvB5gjIu34wXMmv5ue00t8OCXkWs"
 };
 
 /*
