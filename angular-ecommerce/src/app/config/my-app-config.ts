1dfabe6aba3963d73969dc32384f1f0cde50d46f
Oguzz <oguzaslan_203@hotmail.com>
2024-03-14 05:07:52 +0300

    changed http 's ' added

--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -4,7 +4,7 @@ export default {
     oidc: {
         clientId: '0oafpoth2s00FmSfg5d7',
         issuer: 'https://dev-04878644.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
         ClassingEngine: true
     }
