3ef00924ff5a47dd3725742a3acc383ccc30e821
Oguzz <oguzaslan_203@hotmail.com>
2024-04-02 00:59:17 +0300

    production enviroment change

--- angular-ecommerce/src/environments/environment.prod.ts
+++ angular-ecommerce/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  luv2shopApiUrl: 'https://localhost:8443/api',
+  stripePublishableKey: "pk_test_51Ou69UJ4AAVmjh1E3QfZA5Xz9iIZccnwM4xIoID8TjAqV6YqjYmFHax0fG04xJviJH7ERvFvB5gjIu34wXMmv5ue00t8OCXkWs"
+
 };
